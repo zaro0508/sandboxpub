8763c085acb3c38d6c7a4f846da46e9959e0a899
Khai Do <zaro0508@gmail.com>
2017-03-29 16:26:23 -0700

    cleanup travis build

---
+++ travis/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -ex
+# show available schemes
+# xcodebuild -list -project ./Smart4SURE.xcodeproj
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+    fastlane test scheme:"BridgeAppSDK"
+elif [ "$TRAVIS_BRANCH" = "master" ]; then
+    fastlane ci_archive scheme:"BridgeAppSDKSample" export_method:"development"
+fi
+exit $?
