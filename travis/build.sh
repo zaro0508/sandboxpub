0f2fae9e35b524a635a4a4e3bab42cc32b09773a
Khai Do <zaro0508@gmail.com>
2017-03-29 21:49:30 -0700

    fix logic

--- travis/build.sh
+++ travis/build.sh
@@ -2,7 +2,7 @@
 set -ex
 # show available schemes
 # xcodebuild -list -project ./Smart4SURE.xcodeproj
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     fastlane test scheme:"BridgeAppSDK"
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
     fastlane ci_archive scheme:"BridgeAppSDKSample" export_method:"development"
